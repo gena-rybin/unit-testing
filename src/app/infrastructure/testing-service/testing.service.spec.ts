a90aa102b52d926a13bddc744d932b780581f5cb
gena.rybin <genarybin1979@gmail.com>
2023-11-27 12:27:25 +0100

    тест методов шпиона + комментарии

--- src/app/infrastructure/testing-service/testing.service.spec.ts
+++ src/app/infrastructure/testing-service/testing.service.spec.ts
@@ -21,15 +21,43 @@ describe('TestingService', () => {
     expect(service).toBeTruthy();
   });
 
-  it("getValue: should get value by index", () => {
+
+  it("getValue: should get value by index - вызов ориг-го метод", () => {
+    const getValue = service.getValue(1);
+    expect(getValue).toBe("two");
+  });
+
+
+  //разрешили вызов оригинального метода
+  it("getValue: should get value by index - шпион и callThrough", () => {
     // Устанавливаем шпионов - фейк зависимости
     // с фейк вызовами методов вместо оригинальных вызовов
 
     // установили шпиона и разрешили вызов ориг функции
-    // spyOn(firstDependency, "returnValue").and.callThrough();
+    spyOn(firstDependency, "returnValue").and.callThrough();
+
+    const getValue = service.getValue(1);
+    expect(getValue).toBe("two");
+  });
+
+
+  //БЕЗ вызова оригинального метода + можно добавить логику!
+  it("getValue: should get value by index - шпион и callFake", () => {
+    // Устанавливаем шпионов - фейк зависимости
+    // с фейк вызовами методов вместо оригинальных вызовов
 
     // установили шпиона и вызвали фейк функцию = возвращаем метод
-    // spyOn(firstDependency, "returnValue").and.callFake(() => 'two');
+    spyOn(firstDependency, "returnValue").and.callFake(() => 'two');
+
+    const getValue = service.getValue(1);
+    expect(getValue).toBe("two");
+  });
+
+
+  //БЕЗ вызова оригинального метода
+  it("getValue: should get value by index - шпион и returnValue", () => {
+    // Устанавливаем шпионов - фейк зависимости
+    // с фейк вызовами методов вместо оригинальных вызовов
 
     // шпион возвращает только ожидаемое значение
     spyOn(firstDependency, "returnValue").and.returnValue('two');
