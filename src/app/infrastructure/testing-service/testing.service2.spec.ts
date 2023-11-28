e810985dd3bb52aa5af8a9f6f9b9d7461628c531
gena.rybin <genarybin1979@gmail.com>
2023-11-28 11:46:40 +0100

    присваиваем дефолтное значение в блоке beforeEach, не в объявлении. Иначе м.б. flucky test

--- src/app/infrastructure/testing-service/testing.service2.spec.ts
+++ src/app/infrastructure/testing-service/testing.service2.spec.ts
@@ -2,11 +2,11 @@ import {TestingService} from "./testing.service";
 import {TestBed} from "@angular/core/testing";
 import {FirstDependencyService} from "../first-dependency/first-dependency.service";
 
-describe('TestingService', () => {
+describe('TestingService v.2', () => {
   let service: TestingService;
   let firstDependency: FirstDependencyService;
   const fakeFirstDependencyService = {
-    returnValue: jasmine.createSpy('returnValue').and.returnValue("three"),
+    returnValue: jasmine.createSpy('returnValue'),
     initValue: jasmine.createSpy('initValue'),
     initValue2: jasmine.createSpy('initValue2'),
   };
@@ -21,6 +21,7 @@ describe('TestingService', () => {
 
     service = TestBed.inject(TestingService);
     firstDependency = TestBed.inject(FirstDependencyService);
+    fakeFirstDependencyService.returnValue.and.returnValue("three");
   });
 
   // не используем экземпляр класса, а СДЕЛАЕМ ЗАГЛУШКУ ЗАВИСИМОСТИ!
@@ -29,5 +30,11 @@ describe('TestingService', () => {
     expect(service).toBeTruthy();
   });
 
+
+  it('getIndex: should return value по индексу 1', () => {
+    fakeFirstDependencyService.returnValue.and.returnValue('two');
+    const result = service.getValue(service.getIndex());
+    expect(result).toBe('two');
+  });
 
 });
