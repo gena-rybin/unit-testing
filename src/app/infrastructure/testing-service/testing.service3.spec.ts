c6ac25446a76905a241233255246381a08b20b1c
gena.rybin <genarybin1979@gmail.com>
2023-11-29 17:16:50 +0100

    CreateSpyObj - лучший метод мокания зависимостей

---
+++ src/app/infrastructure/testing-service/testing.service3.spec.ts
@@ -0,0 +1,34 @@
+import {TestingService} from "./testing.service";
+import {TestBed} from "@angular/core/testing";
+import {FirstDependencyService} from "../first-dependency/first-dependency.service";
+
+describe('TestingService v.3', () => {
+  let service: TestingService;
+  let firstDependency: FirstDependencyService;
+  const fakeFirstDependencyService = jasmine.createSpyObj(['returnValue', 'initValue', 'initValue2']);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TestingService,
+        {provide: FirstDependencyService, useValue: fakeFirstDependencyService}
+      ],
+    });
+
+    service = TestBed.inject(TestingService);
+    firstDependency = TestBed.inject(FirstDependencyService);
+    fakeFirstDependencyService.returnValue.and.returnValue("three");
+  });
+
+  // CreateSpyObj - лучший метод мокания зависимостей
+
+  it("should creat", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('getIndex: should return value по индексу 1', () => {
+    fakeFirstDependencyService.returnValue.and.returnValue('two');
+    const result = service.getValue(service.getIndex());
+    expect(result).toBe('two');
+  });
+});
